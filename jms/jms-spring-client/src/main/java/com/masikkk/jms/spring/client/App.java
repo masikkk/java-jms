841e9319fef1026ca8dfea89a6b6a5b95166396b
masikkk <masi.1989@163.com>
2017-08-30 16:14:09 +0800

    新建子模块jms-spring-client

---
+++ jms/jms-spring-client/src/main/java/com/masikkk/jms/spring/client/App.java
@@ -0,0 +1,13 @@
+package com.masikkk.jms.spring.client;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
